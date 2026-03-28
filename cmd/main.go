a42998fe46f59823e2e0eb8f6087dcf23c39c946
Ilya Bobrov <ibobrov@ecom.tech>
2026-03-28 13:11:35 +0300

    Вернул Makefile

--- cmd/main.go
+++ cmd/main.go
@@ -1,41 +1,9 @@
 package main
 
 import (
-	"context"
-	"github.com/gofiber/fiber/v2"
-	"job4j.ru/go-lang-base/internal/api"
-	"job4j.ru/go-lang-base/internal/config"
-	"job4j.ru/go-lang-base/internal/db"
-	item "job4j.ru/go-lang-base/internal/repository"
-	"log"
+	"fmt"
 )
 
 func main() {
-	ctx := context.Background()
-
-	cfg := db.Config{
-		Host:     config.Env("DB_HOST", "localhost"),
-		Port:     config.EnvInt("DB_PORT", 6543),
-		User:     config.Env("DB_USER", "postgres"),
-		Password: config.Env("DB_PASSWORD", "password"),
-		DBName:   config.Env("DB_NAME", "tracker"),
-		SSLMode:  config.Env("DB_SSLMODE", "disable"),
-	}
-
-	pool, err := db.NewPool(ctx, cfg.DSN())
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer pool.Close()
-
-	repo := item.NewRepoPg(pool)
-
-	server := api.NewServer(repo)
-	app := fiber.New()
-	server.Route(app.Group("/api"))
-
-	err = app.Listen(":8080")
-	if err != nil {
-		log.Fatal(err)
-	}
+	fmt.Println("Hello World")
 }
