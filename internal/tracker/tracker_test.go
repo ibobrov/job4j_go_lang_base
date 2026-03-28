ceab260ba3cf684d58362842cc51587c3275ed8b
Ilya Bobrov <ibobrov@ecom.tech>
2026-03-28 13:39:33 +0300

    #505565 Инкапсуляция

---
+++ internal/tracker/tracker_test.go
@@ -0,0 +1,38 @@
+package tracker
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func Test_Add(t *testing.T) {
+	t.Parallel()
+
+	t.Run("добавляем item в tracker, меняем имя, смотрим что вернет name", func(t *testing.T) {
+		t.Parallel()
+
+		item := Item{ID: "id", Name: "name"}
+		tracker := NewTracker()
+		tracker.AddItem(item)
+		item.Name = "new name"
+		rsl := tracker.GetItems()
+
+		assert.Equal(t, len(rsl), 1)
+		assert.Equal(t, rsl[0].ID, "id")
+		assert.Equal(t, rsl[0].Name, "name")
+	})
+
+	t.Run("берем item из tracker, меняем имя, смотрим что вернет name", func(t *testing.T) {
+		t.Parallel()
+
+		item := Item{ID: "id", Name: "name"}
+		tracker := NewTracker()
+		tracker.AddItem(item)
+		rsl := tracker.GetItems()
+		tracker.GetItems()[0].Name = "new name"
+
+		assert.Equal(t, len(rsl), 1)
+		assert.Equal(t, rsl[0].ID, "id")
+		assert.Equal(t, rsl[0].Name, "name")
+	})
+}
