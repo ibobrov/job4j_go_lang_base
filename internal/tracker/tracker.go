919b1de7e1cccdb9c97da7966afe434c6bd89af0
i.bobrov <i.bobrov@itfbgroup.ru>
2026-06-18 19:17:11 +0300

    #505568 База данных | Pgx | Goose

--- internal/tracker/tracker.go
+++ internal/tracker/tracker.go
@@ -1,16 +1,5 @@
 package tracker
 
-import "fmt"
-
-type Item struct {
-	ID   string
-	Name string
-}
-
-func (i Item) toString() string {
-	return fmt.Sprintf("Item{id=%s, name=%s}", i.ID, i.Name)
-}
-
 type Tracker struct {
 	Items []Item
 }
--- internal/tracker/usecase.go
+++ internal/tracker/usecase.go
@@ -1,53 +1,57 @@
 package tracker
 
-import "github.com/google/uuid"
+import (
+	"context"
+	"fmt"
+
+	"github.com/google/uuid"
+)
+
+type Store interface {
+	Create(ctx context.Context, item Item) error
+	List(ctx context.Context) ([]Item, error)
+	Get(ctx context.Context, id string) (Item, error)
+}
 
 type UseCase interface {
-	Done(in Input, out Output, tracker *Tracker)
+	Done(ctx context.Context, in Input, out Output, store Store) error
 }
 
 type AddUseCase struct{}
 
-func (u AddUseCase) Done(in Input, out Output, tracker *Tracker) {
+func (u AddUseCase) Done(
+	ctx context.Context,
+	in Input,
+	out Output,
+	store Store,
+) error {
 	out.Out("enter name:")
 	name := in.Get()
 	id := uuid.New().String()
-	item, err := tracker.AddItem(Item{Name: name, ID: id})
-	if err == nil {
-		out.Out("item was added: " + item.toString())
-	} else {
-		out.Out(err.Error())
+
+	if err := store.Create(
+		ctx,
+		Item{ID: id, Name: name},
+	); err != nil {
+		return fmt.Errorf("failed to create item: %w", err)
 	}
+	return nil
 }
 
 type GetUseCase struct{}
 
-func (u GetUseCase) Done(_ Input, out Output, tracker *Tracker) {
-	for _, item := range tracker.Items {
-		out.Out(item.toString())
+func (u GetUseCase) Done(
+	ctx context.Context,
+	in Input,
+	out Output,
+	store Store,
+) error {
+	items, err := store.List(ctx)
+	if err != nil {
+		return fmt.Errorf("failed to get items: %w", err)
 	}
-}
-
-type UpdateUseCase struct{}
-
-func (u UpdateUseCase) Done(in Input, out Output, tracker *Tracker) {
-	out.Out("enter id:")
-	id := in.Get()
-
-	out.Out("enter new name:")
-	name := in.Get()
-	err := tracker.UpdateItem(Item{Name: name, ID: id})
-	if err == nil {
-		out.Out("item was updated")
-	} else {
-		out.Out(err.Error())
+	for _, item := range items {
+		out.Out(item.ID + " " + item.Name)
 	}
-}
-
-type DeleteUseCase struct{}
-
-func (u DeleteUseCase) Done(in Input, out Output, tracker *Tracker) {
-	out.Out("enter id:")
-	id := in.Get()
-	tracker.DeleteItem(id)
+	return nil
 }
