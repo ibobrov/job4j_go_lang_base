fe82d2012ee76925964232f4d7acfc85c643bede
Ilya Bobrov <ibobrov@ecom.tech>
2026-06-21 12:31:57 +0300

    #505564 Правки после ревью

--- internal/tracker/usecase.go
+++ internal/tracker/usecase.go
@@ -10,7 +10,10 @@ type AddUseCase struct{}
 
 func (u AddUseCase) Done(in Input, out Output, tracker *Tracker) {
 	out.Out("enter name:")
-	name := in.Get()
+	name, err := in.Get()
+	if err != nil {
+		out.Out("Wrong input!")
+	}
 	id := uuid.New().String()
 	item, err := tracker.AddItem(Item{Name: name, ID: id})
 	if err == nil {
@@ -32,11 +35,17 @@ type UpdateUseCase struct{}
 
 func (u UpdateUseCase) Done(in Input, out Output, tracker *Tracker) {
 	out.Out("enter id:")
-	id := in.Get()
+	id, err := in.Get()
+	if err != nil {
+		out.Out("Wrong input!")
+	}
 
 	out.Out("enter new name:")
-	name := in.Get()
-	err := tracker.UpdateItem(Item{Name: name, ID: id})
+	name, err := in.Get()
+	if err != nil {
+		out.Out("Wrong input!")
+	}
+	err = tracker.UpdateItem(Item{Name: name, ID: id})
 	if err == nil {
 		out.Out("item was updated")
 	} else {
@@ -48,6 +57,10 @@ type DeleteUseCase struct{}
 
 func (u DeleteUseCase) Done(in Input, out Output, tracker *Tracker) {
 	out.Out("enter id:")
-	id := in.Get()
+	id, err := in.Get()
+	if err != nil {
+		out.Out("Wrong input!")
+	}
+
 	tracker.DeleteItem(id)
 }
